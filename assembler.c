e4c911efbeb5449010048b77da1cdf233fe9ac24
saumya-prakash <saumyaphonu@gmail.com>
2020-10-29 09:05:03 +0530

    work starts

--- assembler.c
+++ assembler.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
+    if(argc==1)
+    {
+        printf("Fatal error: No input file(s)\n");
+        exit(EXIT_FAILURE);
+    }
 
 
 
-
-    exit(1);
+    exit(EXIT_SUCCESS);
 }
